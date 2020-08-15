606976444df12b7188113ba7f264667fdaa8c09f
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-08-15 21:14:29 +0900

    nonce header & recalculation function

--- client.rb
+++ client.rb
@@ -1,6 +1,8 @@
 class Iamswer::Client
   include Singleton
 
+  NONCE_HEADER = "X-Iamswer-Signature-Nonce".freeze
+
   class << self
     delegate :get,
       :post,
@@ -30,6 +32,16 @@ class Iamswer::Client
     end
 
     JSON.parse(response.body).with_indifferent_access
+  end
+
+  def self.nonce_header
+    NONCE_HEADER
+  end
+
+  def self.calculate_callback_nonce(secret_key, payload)
+    digest = OpenSSL::Digest.new("sha256")
+    hmac = OpenSSL::HMAC.digest(digest, secret_key, payload.to_json)
+    Base64.strict_encode64(hmac)
   end
 
   private
