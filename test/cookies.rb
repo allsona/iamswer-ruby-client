d05de1a5974a9ed139b595e6f5c2c6867a652314
Adam Notodikromo <adamnoto@hey.com>
2021-04-04 16:21:23 +0900

    tailor setting cookies for different browsers

--- test/cookies.rb
+++ test/cookies.rb
@@ -17,7 +17,22 @@ module Iamswer::Test::Cookies
 
     if respond_to?(:page)
       # if entering this block, we are doing end-to-end testing using capybara
-      page.driver.set_cookie session_cookie_name, signed_session_id
+      case page.driver.class.to_s
+      when "Capybara::Apparition::Driver"
+        page.driver.set_cookie session_cookie_name, signed_session_id
+      when "Capybara::Selenium::Driver"
+        if respond_to? :root_path
+          visit root_path
+        else
+          root_path = given_values.delete :initial_path
+          raise Iamswer::Error, "Please provide the initial_path for installing the cookie to avoid domain error" unless root_path
+          visit root_path
+        end
+
+        page.driver.browser.manage.add_cookie name: session_cookie_name, value: signed_session_id
+      else
+        raise Iamswer::Error, "Unknown driver class: #{page.driver.class}. Please report this to the Iamswer team."
+      end
     else
       # if entering this block, we are doing integration testing on the controller
       cookies[session_cookie_name] = Iamswer::SessionHandler.session_verifier.generate(session_id)
